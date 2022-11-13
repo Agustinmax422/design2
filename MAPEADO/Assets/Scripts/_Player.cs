3b6753a5a551f82c83c3fe16f60807729537fc7a
Kevin <kalexisllampa42034@gmail.com>
2022-11-13 03:48:47 -0300

    add_fonts_Update_player-scenes

--- MAPEADO/Assets/Scripts/_Player.cs
+++ MAPEADO/Assets/Scripts/_Player.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class _Player : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 30f;
     private new Rigidbody rigidbody;
+    public float sensitivity = 2;
+    public float smoothing = 1.5f;
+    private Vector2 velocity;
+    private Vector2 frameVelocity;
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
         rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        _Move();
+        _look();
+    }
+  
+    private void _Move()
     {
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
@@ -25,4 +35,13 @@ public class _Player : MonoBehaviour
             rigidbody.MovePosition(transform.position + dir * speed * Time.deltaTime);
         }
     }
+    private void _look()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
+        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+        velocity += frameVelocity;
+        transform.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+    }
 }
+
